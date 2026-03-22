4b6cad09a5c181b32a6186b74d55b9401c4e7294
jer <mjlerba@gmail.com>
2026-03-22 20:08:03 +0200

    feat: url logout redirect fix

--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL= '/accounts/login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
