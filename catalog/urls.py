b031512a7c2eed425c86c945a1c40aa097765439
monamijer <mjlerba@gmail.com>
2026-03-23 06:32:58 +0000

    feat fix and correct views urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -18,6 +18,5 @@ urlpatterns = [
         path('book/create/', views.BookCreate.as_view(), name='book-create'),
         path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
         path('book/<int:pk>/delete/', views.BookDelete.as_view(), name='book-delete'),
-        path('deploy/', deploy),
 
         ]
